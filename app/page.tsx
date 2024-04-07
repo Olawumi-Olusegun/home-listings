1b219b3a415a25b720652f475233e748e81b82c4
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-04-07 10:22:09 +0100

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import MapFilterItems from "./components/MapFilterItems";
 import prismaDb from "./lib/db";
 import { unstable_noStore as noStore } from "next/cache";
 import SkeletonLoading from "./components/SkeletonLoading";
-import ShowItems from "./components/ShowItems";
+import NoItem from "./components/NoItem";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import ListingCard from "./components/ListingCard";
 
 type getDataSearchParamsProps = {
   searchParams?: { 
@@ -71,5 +73,35 @@ export default function Home({searchParams}: searchParamsProps) {
     </Suspense>
    </main>
   );
+}
+
+
+async function ShowItems({searchParams}: searchParamsProps) {
+
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+
+  const data = await getData({searchParams: searchParams, userId: user?.id});
+
+  if(!data || data.length === 0) {
+    return <NoItem />
+  }
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-8 ">
+    {data.map((item) => <ListingCard key={item.id}  
+        imagePath = {item.photo as string}
+        price = {item.price as number}
+        description = {item.description as string}
+        location = {item.country as string}
+        userId={user?.id}
+        favouriteId={item.favourites[0]?.id}
+        isInFavouriteList={item.favourites.length > 0 }
+        homeId={item.id}
+        pathName="/"
+     />)}
+  </div>
+  )
 }
 
